14e539b46ecc7e68a3fd80d50939dbf5805ab872
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-07 00:51:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-   {:railroads => {
+   {:railroads => {:names => {
       :rent_in_dollars => 
       { one: " ", two: " ", three: " ", four: " "}
     } 

a2df1e0b488cc332086153847fbb2c2edf70d3b2
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-07 01:14:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_fourth_tier
    {
      :railroads => {
       :names => { 
-        :reading_railroad => { "$mortgage_value" $100 },
+        :reading_railroad => {  "$mortgage_value" $100},
         :pennsylvania_railroad => { "$mortgage_value" $100 },
         :b_and_o_railroad => { mortgage_value: "$100" }, 
         :shortline_railroad => { mortgage_value: "$100" } 

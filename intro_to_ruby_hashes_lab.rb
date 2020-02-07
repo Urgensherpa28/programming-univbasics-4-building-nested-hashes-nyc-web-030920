abe4e8e5257c9fdf52bf518999c69027d481059e
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-07 00:58:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
    {
      :railroads => {
       :names => { one: " ", two: " ", three: " ", four: " " }, 
-      :rent_in_dollars => { one_piece_owned: 25, two: " ", three: " ", four: " "}
+      :rent_in_dollars => { one_piece_owned: 25, two_pieces_owned: 50, three: " ", four: " "}
     } 
    }
    
